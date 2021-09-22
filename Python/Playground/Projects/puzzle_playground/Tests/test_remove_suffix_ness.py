4b178c46fe5e1700a032e2648d0e92b47b9ff566
Cole Boren <william.boren.osv@fedex.com>
2021-09-22 13:46:21 -0500

    Test for checking puzzle

---
+++ Python/Playground/Projects/puzzle_playground/Tests/test_remove_suffix_ness.py
@@ -0,0 +1,15 @@
+import unittest
+
+from Projects.puzzle_playground.puzzles.remove_suffix_ness import remove_suffix_ness
+
+
+class MyTestCase(unittest.TestCase):
+    def test_one(self):
+        input_data = ["heaviness", "sadness", "softness", "crabbiness", "lightness", "artiness", "edginess"]
+        result_data = ["heavy", "sad", "soft", 'crabby', 'light', 'arty', 'edgy']
+        number_of_variants = range(1, len(input_data) + 1)
+
+        for variant, word, result in zip(number_of_variants, input_data, result_data):
+            with self.subTest(f"variation #{variant}", word=word, result=result):
+                self.assertEqual(remove_suffix_ness(word), result,
+                                 msg=f'Expected: {result} but got a different word instead.')
