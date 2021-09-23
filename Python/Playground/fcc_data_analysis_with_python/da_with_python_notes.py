cde85cd5e75f8187347cbc7967e5a03c267bb00f
Cole Boren <william.boren.osv@fedex.com>
2021-09-23 13:22:58 -0500

    backing up fcc work

---
+++ Python/Playground/fcc_data_analysis_with_python/da_with_python_notes.py
@@ -0,0 +1,2 @@
+# data analysis -> process of inspecting, cleansing, transforming, and modeling data with the goal
+# of discovering useful information to make wise decisions(transform data into information)
