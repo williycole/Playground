5d38aaa811c7f712fcde4bc708444ab3167db7ee
williycole <64283810+williycole@users.noreply.github.com>
2021-10-05 14:37:13 -0500

    Adding in gitignore place holder and C# playground

---
+++ C-Sharp/Puzzles/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static DoesContainDuplicates;
+
+namespace Puzzles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        IsIsogram("Dermatoglyphics");
+        IsIsogram("isogram");
+        IsIsogram("moose");
+        IsIsogram("isIsogram");
+        IsIsogram("aba");
+        IsIsogram("moOse");
+        IsIsogram("thumbscrewjapingly");
+        IsIsogram("");
+        IsIsogram("boot");
+        }
+    }
+}
